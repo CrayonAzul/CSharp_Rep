172a953eed1823d499b604a6a4cc7168c802c1b9
CrayonAzul <91706813+CrayonAzul@users.noreply.github.com>
2023-06-17 14:44:31 -0700

    Create Unit3.cs

---
+++ easy/Windows_Learn_Training/Challenge/Store and retrieve/Unit3.cs
@@ -0,0 +1,10 @@
+Console.WriteLine('b');
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39616m);
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
